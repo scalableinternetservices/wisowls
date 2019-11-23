80bf36976574ee6cc9f87e26f0ebde8379b8dbf3
Will Htun <willhtun42@gmail.com>
2019-11-23 00:34:21 +0000

    Profile edit fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :zipcode, :email, :password,
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation, :image)
     end
     
