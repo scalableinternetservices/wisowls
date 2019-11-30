a03c3d895d534ac6cd49296ebca349e877054f46
Will Htun <willhtun42@gmail.com>
2019-11-30 02:41:07 +0000

    Fixed zipcode in controller, filter front end and css

--- app/controllers/matcher_controller.rb
+++ app/controllers/matcher_controller.rb
@@ -1,28 +1,37 @@
 class MatcherController < ApplicationController
   def home
-  require "net/http"
-  require "uri"
-  require "json"
-  #require "httparty"
+    require "net/http"
+    require "uri"
+    require "json"
+    #require "httparty"
   
-    @alldogs = Dog.all
+    allDogs = Dog.all
     @dogs = []
-    like_radius = params['radius'].to_i
     
-     # Replace 90024 with my_zip 
-     for dog in @alldogs
-      other_zip = dog.zipcode
-      zipcode_url = "https://www.zipcodeapi.com/rest/GmiyTSTTuReA0ppnY5NodOpUIjuC1TBX4zf9zVGMsYjkReSFb8AJLdhSJAHqn27M/distance.json/" + "90024" + "/" + other_zip.to_s + "/mile"
-      uri = URI.parse(zipcode_url)
-      httpResponse = Net::HTTP.get_response(uri)
-      zipDistance = JSON.parse(httpResponse.body)
-
-      if like_radius < zipDistance['distance'].to_i
-        @dogs << :dog
-      end
-      
+    if (params[:matcher].present? && params[:matcher][:radius].present?)
+      like_radius = params[:matcher][:radius].to_i
+    else
+      like_radius = 0
     end
-
+    
+    if (like_radius == 0)
+      @dogs = allDogs
+    else
+      dogList = []
+      for dog in allDogs
+        other_zip = dog.zipcode
+        zipcode_url = "https://www.zipcodeapi.com/rest/GmiyTSTTuReA0ppnY5NodOpUIjuC1TBX4zf9zVGMsYjkReSFb8AJLdhSJAHqn27M/distance.json/" + "90024" + "/" + other_zip.to_s + "/mile"
+        uri = URI.parse(zipcode_url)
+        httpResponse = Net::HTTP.get_response(uri)
+        zipDistance = JSON.parse(httpResponse.body)
+  
+        dist = (other_zip.to_i - Dog.find(current_user.id).zipcode.to_i).abs
+        if dist < like_radius.to_i
+          dogList.append(dog)
+        end
+        end
+      @dogs = dogList
+    end
     
     sender = User.find(current_user.id)
     convos = current_user.mailbox.conversations
