7454baed7fbb0003b243bc097d8ba5921f9654eb
Will Htun <willhtun42@gmail.com>
2019-12-04 04:48:18 +0000

    More dynamic seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users_data = [
-  [ "Ali", "ali@gmail.com", "password" ],
-  [ "Alley", "alley@gmail.com", "password" ],
-  [ "Beth", "beth@gmail.com", "password" ],
-  [ "Caren", "caren@gmail.com", "password" ],
-  [ "Danny", "dani@gmail.com", "password" ],
-  [ "Emad", "emad@gmail.com", "password" ],
-  [ "Fifi", "fifi@gmail.com", "password" ],
-  [ "Guy", "guy@gmail.com", "password" ]
-]
-
-dogs_data = [
-  ["Emma", 4, "Female", "Pit", 15, 90021],
-  ["Archie", 6, "Male", "Lab", 10, 90096],
-  ["Rocky", 10, "Male", "Samoyed", 25, 90032],
-  ["Baddie", 2, "Male", "Pit Terrier", 8, 91010],
-  ["Tucker", 6, "Male", "Pit", 15, 90076],
-  ["Emily", 5, "Female", "Pit", 15, 90101],
-  ["Barnie", 7, "Male", "Pit", 15, 90101],
-  ["Rorry", 5, "Male", "Pit", 15, 90071],
-]
-
-users_data.each do |name, email, password|
-  User.create(name: name, email: email, password_digest: password)
-end
-
-dogs_data.each do |name, age, gender, breed, weight, zipcode|
-  Dog.create( name: name, age: age, gender: gender, breed: breed, weight: weight, zipcode: zipcode)
+i = 0
+loop do
+  User.create(name: "user_" + i.to_s, email: "user_" + i.to_s + "@gmail.com", password_digest: "password123")
+  Dog.create( name: "dog_" + i.to_s, age: "10", gender: "Male", breed: "Random Breed", weight: "50", zipcode: (90000 + i).to_s)
+  if i == 100
+    break
+  end
+  i += 1
 end
